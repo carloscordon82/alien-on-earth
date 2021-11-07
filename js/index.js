a763d75ccd604a96f38b6f29c366bcddfa1b4b65
Carlos Cordon <carloscordon82@gmail.com>
2021-11-07 18:32:07 -0500

    Testing Motion

---
+++ js/index.js
@@ -0,0 +1,354 @@
+let globalX = 0;
+let globalY = 0;
+let globalSpeed = 0;
+let trueDirection = "";
+let mousex = 0;
+let mousey = 0;
+var drag = false;
+var dragStart;
+var dragEnd;
+
+class Sprite {
+  constructor(x, y, width, height, image, speed) {
+    this.x = x;
+    this.y = y;
+    this.width = width;
+    this.height = height;
+    this.image = image;
+    this.speed = speed;
+  }
+
+  draw() {
+    ctx.drawImage(
+      this.image,
+      this.x + globalX,
+      this.y + globalY,
+      this.width,
+      this.height
+    );
+  }
+
+  loadImage() {
+    const newImage = new Image();
+    newImage.src = this.image;
+    this.image = newImage;
+  }
+}
+
+class Block extends Sprite {
+  constructor(x, y, width, height, image, mass) {
+    super(x, y, width, height, image, mass);
+    this.mass = mass;
+  }
+}
+
+class Player extends Sprite {
+  constructor(x, y, width, height, imageGroup, speed) {
+    super(x, y, width, height, "", speed);
+    this.accelerateUp = 0;
+    this.accelerateDown = 0;
+    this.allowedToJump = true;
+    this.jumping = false;
+    this.direction = "stopped_right";
+    this.imageGroup = imageGroup;
+    this.stepsRight = 0;
+    this.stepsLeft = 0;
+    this.intent = "";
+  }
+
+  draw() {
+    if (this.direction === "right")
+      if (!this.jumping) {
+        this.stepsLeft = 0;
+        if (this.stepsRight === this.imageGroup.right.length - 1) {
+          this.stepsRight = 0;
+        } else {
+          this.stepsRight++;
+        }
+        ctx.drawImage(
+          this.imageGroup.right[this.stepsRight],
+          this.x + globalX,
+          this.y + globalY,
+          this.width,
+          this.height
+        );
+      } else
+        ctx.drawImage(
+          this.imageGroup.jumping_right,
+          this.x + globalX,
+          this.y + globalY,
+          this.width,
+          this.height
+        );
+
+    if (this.direction === "left")
+      if (!this.jumping) {
+        this.stepsRight = 0;
+        if (this.stepsLeft === this.imageGroup.left.length - 1) {
+          this.stepsLeft = 0;
+        } else {
+          this.stepsLeft++;
+        }
+
+        ctx.drawImage(
+          this.imageGroup.left[this.stepsLeft],
+          this.x + globalX,
+          this.y + globalY,
+          this.width,
+          this.height
+        );
+      } else
+        ctx.drawImage(
+          this.imageGroup.jumping_left,
+          this.x + globalX,
+          this.y + globalY,
+          this.width,
+          this.height
+        );
+
+    if (this.direction === "stopped_left") {
+      ctx.drawImage(
+        this.imageGroup.stopped_left,
+        this.x + globalX,
+        this.y + globalY,
+        this.width,
+        this.height
+      );
+    }
+
+    if (this.direction === "stopped_right") {
+      ctx.drawImage(
+        this.imageGroup.stopped_right,
+        this.x + globalX,
+        this.y + globalY,
+        this.width,
+        this.height
+      );
+    }
+  }
+
+  loadImage() {
+    for (i = 0; i < this.imageGroup.right.length; i++) {
+      let newImage = new Image();
+      newImage.src = this.imageGroup.right[i];
+      this.imageGroup.right[i] = newImage;
+    }
+
+    for (i = 0; i < this.imageGroup.left.length; i++) {
+      let newImage = new Image();
+      newImage.src = this.imageGroup.left[i];
+      this.imageGroup.left[i] = newImage;
+    }
+
+    let newImageRight = new Image();
+    newImageRight.src = this.imageGroup.stopped_left;
+    this.imageGroup.stopped_left = newImageRight;
+
+    let newImageLeft = new Image();
+    newImageLeft.src = this.imageGroup.stopped_right;
+    this.imageGroup.stopped_right = newImageLeft;
+
+    let newImageJumpLeft = new Image();
+    newImageJumpLeft.src = this.imageGroup.jumping_left;
+    this.imageGroup.jumping_left = newImageJumpLeft;
+
+    let newImageJumpRight = new Image();
+    newImageJumpRight.src = this.imageGroup.jumping_right;
+    this.imageGroup.jumping_right = newImageJumpRight;
+  }
+
+  moving(e) {
+    if (e.keyCode === 37) {
+      this.direction = "left";
+      trueDirection = "left";
+      this.speed = -10;
+    }
+    if (e.keyCode === 39) {
+      this.direction = "right";
+      trueDirection = "right";
+      this.speed = 10;
+    }
+  }
+
+  Jump() {
+    this.y += this.accelerateUp;
+    this.accelerateUp += this.accelerateDown;
+  }
+
+  stop() {
+    this.speed = 0;
+    if (this.direction === "right") this.direction = "stopped_right";
+    if (this.direction === "left") this.direction = "stopped_left";
+  }
+}
+
+/////// START OF CODE
+
+let canvas = document.getElementById("canvas");
+ctx = canvas.getContext("2d");
+let tempGlobal = 0;
+
+canvas.addEventListener("mousedown", function (event) {
+  dragStart = {
+    mousex: event.pageX - canvas.offsetLeft,
+    mousey: event.pageY - canvas.offsetTop,
+  };
+  tempGlobal = globalX;
+  console.log("assigning");
+  drag = true;
+});
+
+canvas.addEventListener("mouseup", function (event) {
+  drag = false;
+});
+
+canvas.addEventListener("mousemove", function (event) {
+  if (drag) {
+    dragEnd = {
+      mousex: event.pageX - canvas.offsetLeft,
+      mousey: event.pageY - canvas.offsetTop,
+    };
+    ctx.translate(dragEnd.x - dragStart.x, dragEnd.y - dragStart.y);
+    globalX = tempGlobal + dragEnd.mousex - dragStart.mousex;
+  }
+});
+
+let images = [[], [], [], [], [], [], [], [], [], []];
+for (i = 0; i < land.length; i++) {
+  for (j = 0; j < land[i].length; j++) {
+    images[i].push(
+      new Block(j * 70, i * 70, 70, 70, land[i][j].image, land[i][j].mass)
+    );
+    images[i][j].loadImage();
+  }
+}
+let player1 = new Player(200, 320, 72, 97, player, 0);
+player1.loadImage();
+let tempMove = false;
+document.addEventListener("keydown", (e) => {
+  tempMove = true;
+  player1.moving(e);
+  if (e.keyCode === 38 && player1.allowedToJump) {
+    player1.jumping = true;
+    player1.allowedToJump = false;
+    player1.accelerateDown = 1;
+    player1.accelerateUp = -15;
+  }
+});
+document.addEventListener("keyup", (e) => {
+  if (e.keyCode === 37 || e.keyCode === 39) {
+    player1.stop();
+    movingLandStop();
+    trueDirection = "";
+  }
+});
+
+function moveLand(player1SpeedDirection) {
+  if (player1SpeedDirection > 0) {
+    if (player1.x + globalX > 600) {
+      globalSpeed = -player1.speed;
+    }
+  }
+  if (player1SpeedDirection < 0) {
+    if (player1.x + globalX < 200) {
+      globalSpeed = -player1.speed;
+    }
+  }
+}
+
+function movingLandStop() {
+  globalSpeed = 0;
+}
+function updateCanvas() {
+  if (trueDirection === "right") {
+    player1.moving({ keyCode: 39 });
+  }
+
+  if (trueDirection === "left") {
+    player1.moving({ keyCode: 37 });
+  }
+
+  if (player1.direction === "right")
+    if (
+      images[Math.floor(player1.y / 70)][
+        Math.floor((player1.x + player1.width) / 70)
+      ].mass === "solid" ||
+      images[Math.floor((player1.y + player1.height) / 70)][
+        Math.floor((player1.x + player1.width) / 70)
+      ].mass === "solid" ||
+      images[Math.floor((player1.y + player1.height / 2) / 70)][
+        Math.floor((player1.x + player1.width) / 70)
+      ].mass === "solid"
+    ) {
+      player1.stop();
+      movingLandStop();
+    }
+  if (player1.direction === "left")
+    if (
+      images[Math.floor(player1.y / 70)][Math.floor((player1.x - 5) / 70)]
+        .mass === "solid" ||
+      images[Math.floor((player1.y + player1.height) / 70)][
+        Math.floor((player1.x - 5) / 70)
+      ].mass === "solid" ||
+      images[Math.floor((player1.y + player1.height / 2) / 70)][
+        Math.floor((player1.x - 5) / 70)
+      ].mass === "solid"
+    ) {
+      player1.stop();
+      movingLandStop();
+    }
+
+  if (player1.jumping)
+    if (
+      images[Math.floor((player1.y + player1.height) / 70)][
+        // THE 5 HERE IS CUSHION FOR PLAYER
+        Math.floor((player1.x + 5) / 70)
+      ].mass === "solid" ||
+      images[Math.floor((player1.y + player1.height) / 70)][
+        Math.floor((player1.x + player1.width - 5) / 70)
+      ].mass === "solid"
+    ) {
+      // MODIFY THIS INTO ITS OWN FUNCTION
+      player1.jumping = false;
+      player1.allowedToJump = true;
+      player1.accelerateUp = 0;
+      player1.accelerateDown = 0;
+      player1.allowedToJump = true;
+      player1.y =
+        images[Math.floor((player1.y + player1.height) / 70)][
+          Math.floor(player1.x / 70)
+        ].y -
+        player1.height -
+        1;
+      movingLandStop();
+    }
+  if (
+    !player1.jumping &&
+    images[Math.floor((player1.y + player1.height + 1) / 70)][
+      Math.floor(player1.x / 70)
+    ].mass === "air" &&
+    images[Math.floor((player1.y + player1.height + 1) / 70)][
+      Math.floor((player1.x + player1.width) / 70)
+    ].mass === "air"
+  ) {
+    player1.jumping = true;
+    player1.allowedToJump = false;
+    player1.accelerateUp = 0;
+    player1.accelerateDown = 1;
+  }
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+  for (i = 0; i < land.length; i++) {
+    for (j = 0; j < land[i].length; j++) {
+      images[i][j].draw();
+    }
+  }
+
+  player1.x += player1.speed;
+  player1.Jump();
+  globalX += globalSpeed;
+  moveLand(player1.speed);
+  player1.draw();
+  requestAnimationFrame(updateCanvas);
+}
+updateCanvas();
---
+++ js/world.js
@@ -0,0 +1,54 @@
+let block1 = { image: "images/block1.png", mass: "solid" };
+let block2 = { image: "images/block2.png", mass: "solid" };
+let block3 = { image: "images/block3.png", mass: "solid" };
+let block4 = { image: "images/block4.png", mass: "solid" };
+let block5 = { image: "images/block5.png", mass: "air" };
+let player = {
+  right: [
+    "images/p1_walk01_right.png",
+    "images/p1_walk02_right.png",
+    "images/p1_walk03_right.png",
+    "images/p1_walk04_right.png",
+    "images/p1_walk05_right.png",
+    "images/p1_walk06_right.png",
+    "images/p1_walk07_right.png",
+    "images/p1_walk08_right.png",
+    "images/p1_walk09_right.png",
+    "images/p1_walk10_right.png",
+    "images/p1_walk11_right.png",
+    "images/p1_walk10_right.png",
+    "images/p1_walk11_right.png",
+  ],
+  left: [
+    "images/p1_walk01_left.png",
+    "images/p1_walk02_left.png",
+    "images/p1_walk03_left.png",
+    "images/p1_walk04_left.png",
+    "images/p1_walk05_left.png",
+    "images/p1_walk06_left.png",
+    "images/p1_walk07_left.png",
+    "images/p1_walk08_left.png",
+    "images/p1_walk09_left.png",
+    "images/p1_walk10_left.png",
+    "images/p1_walk11_left.png",
+    "images/p1_walk10_left.png",
+    "images/p1_walk11_left.png",
+  ],
+  stopped_right: "images/p1_stand_right.png",
+  stopped_left: "images/p1_stand_left.png",
+  jumping_right: "images/p1_jumping_right.png",
+  jumping_left: "images/p1_jumping_left.png",
+};
+
+// prettier-ignore
+let land = [
+  [block3, block5, block5, block5, block5, block5, block5, block5, block5, block5, block5, block5, block5, block5, block5, block5, block5, block5, block5, block5, block5, block5, block5, block5, block5, block5, block5, block5, block5, block5, block5, block5, block3],
+  [block3, block5, block5, block5, block5, block5, block5, block5, block5, block5, block5, block5, block5, block5, block5, block5, block5, block5, block5, block5, block5, block5, block5, block5, block5, block5, block5, block5, block5, block5, block5, block5, block3],
+  [block3, block5, block5, block5, block5, block5, block5, block5, block5, block5, block5, block5, block5, block5, block5, block5, block5, block5, block5, block5, block5, block5, block5, block5, block5, block5, block5, block5, block5, block5, block5, block5, block3],
+  [block3, block5, block5, block5, block5, block5, block5, block5, block5, block5, block5, block5, block5, block5, block5, block5, block5, block5, block5, block5, block5, block5, block5, block5, block5, block5, block5, block5, block5, block5, block5, block5, block3],
+  [block3, block5, block5, block5, block5, block5, block5, block5, block5, block5, block3, block3, block3, block5, block5, block5, block5, block5, block5, block5, block5, block5, block5, block5, block5, block5, block5, block5, block5, block5, block5, block5, block3],
+  [block3, block5, block5, block5, block5, block5, block5, block5, block5, block3, block5, block5, block5, block3, block5, block5, block5, block5, block5, block5, block5, block5, block5, block5, block5, block5, block5, block5, block5, block5, block5, block5, block3],
+  [block3, block5, block5, block5, block5, block5, block5, block3, block3, block5, block5, block5, block5, block5, block3, block5, block5, block5, block5, block5, block5, block5, block5, block5, block5, block5, block5, block5, block5, block5, block5, block5, block3],
+  [block3, block3, block3, block3, block3, block3, block3, block3, block3, block3, block3, block3, block3, block3, block3, block3, block3, block3, block3, block3, block3, block3, block3, block3, block3, block3, block3, block3, block3, block3, block3, block3, block3],
+  [block3, block4, block4, block4, block4, block4, block4, block4, block4, block4, block4, block4, block4, block4, block4, block4, block4, block4, block4, block4, block4, block4, block4, block4, block4, block4, block4, block4, block4, block4, block4, block4, block4],
+];
