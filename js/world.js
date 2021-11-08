df204a21d65ef80885ca072521728d5491b13732
Carlos Cordon <carloscordon82@gmail.com>
2021-11-07 19:18:23 -0500

    Added some Green

--- js/world.js
+++ js/world.js
@@ -3,6 +3,11 @@ let block2 = { image: "images/block2.png", mass: "solid" };
 let block3 = { image: "images/block3.png", mass: "solid" };
 let block4 = { image: "images/block4.png", mass: "solid" };
 let block5 = { image: "images/block5.png", mass: "air" };
+let block6 = { image: "images/grassLeft.png", mass: "solid" };
+let block7 = { image: "images/grassMid.png", mass: "solid" };
+let block8 = { image: "images/grassRight.png", mass: "solid" };
+let block9 = { image: "images/grassCenter.png", mass: "solid" };
+
 let player = {
   right: [
     "images/p1_walk01_right.png",
@@ -46,9 +51,9 @@ let land = [
   [block3, block5, block5, block5, block5, block5, block5, block5, block5, block5, block5, block5, block5, block5, block5, block5, block5, block5, block5, block5, block5, block5, block5, block5, block5, block5, block5, block5, block5, block5, block5, block5, block3],
   [block3, block5, block5, block5, block5, block5, block5, block5, block5, block5, block5, block5, block5, block5, block5, block5, block5, block5, block5, block5, block5, block5, block5, block5, block5, block5, block5, block5, block5, block5, block5, block5, block3],
   [block3, block5, block5, block5, block5, block5, block5, block5, block5, block5, block5, block5, block5, block5, block5, block5, block5, block5, block5, block5, block5, block5, block5, block5, block5, block5, block5, block5, block5, block5, block5, block5, block3],
-  [block3, block5, block5, block5, block5, block5, block5, block5, block5, block5, block3, block3, block3, block5, block5, block5, block5, block5, block5, block5, block5, block5, block5, block5, block5, block5, block5, block5, block5, block5, block5, block5, block3],
-  [block3, block5, block5, block5, block5, block5, block5, block5, block5, block3, block5, block5, block5, block3, block5, block5, block5, block5, block5, block5, block5, block5, block5, block5, block5, block5, block5, block5, block5, block5, block5, block5, block3],
-  [block3, block5, block5, block5, block5, block5, block5, block3, block3, block5, block5, block5, block5, block5, block3, block5, block5, block5, block5, block5, block5, block5, block5, block5, block5, block5, block5, block5, block5, block5, block5, block5, block3],
-  [block3, block3, block3, block3, block3, block3, block3, block3, block3, block3, block3, block3, block3, block3, block3, block3, block3, block3, block3, block3, block3, block3, block3, block3, block3, block3, block3, block3, block3, block3, block3, block3, block3],
-  [block3, block4, block4, block4, block4, block4, block4, block4, block4, block4, block4, block4, block4, block4, block4, block4, block4, block4, block4, block4, block4, block4, block4, block4, block4, block4, block4, block4, block4, block4, block4, block4, block4],
+  [block3, block5, block5, block5, block5, block5, block5, block5, block5, block5, block6, block7, block8, block5, block5, block5, block5, block5, block5, block5, block5, block5, block5, block5, block5, block5, block5, block5, block5, block5, block5, block5, block3],
+  [block3, block5, block5, block5, block5, block5, block5, block5, block6, block7, block8, block5, block6, block7, block7, block8, block5, block5, block5, block5, block5, block5, block5, block5, block5, block5, block5, block5, block5, block5, block5, block5, block3],
+  [block3, block5, block5, block5, block5, block5, block5, block6, block7, block7, block8, block5, block5, block5, block7, block7, block8, block5, block5, block5, block5, block5, block5, block5, block5, block5, block5, block5, block5, block5, block5, block5, block3],
+  [block7, block7, block7, block7, block7, block7, block7, block7, block7, block7, block7, block8, block5, block6, block7, block7, block7, block5, block7, block7, block7, block7, block7, block7, block7, block5, block5, block7, block7, block7, block7, block7, block7],
+  [block9, block9, block9, block9, block9, block9, block9, block9, block9, block9, block9, block9, block5, block9, block9, block9, block9, block5, block9, block9, block9, block9, block9, block9, block9, block5, block5, block9, block9, block9, block9, block9, block9],
 ];
